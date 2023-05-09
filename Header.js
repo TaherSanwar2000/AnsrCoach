10267224cf4ad3e3cfa2566da48029c9cf3ddef3
Tahersanwar <tahersanwar53@gmail.com>
2023-05-09 16:43:49 +0530

    AnsrCoach Assignment

--- Header.js
+++ Header.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { AntDesign } from '@expo/vector-icons';
 
 const Header = (props) => {
   return (
-    <View style={{margin:15}}>
-      <Text style={{fontSize:30,fontWeight:'bold',color:'white',marginLeft:100}}>{props.name}</Text>
+    <View style={{margin:15,flexDirection:'row'}}>
+       <AntDesign name='arrowleft' size={40} color='white'/>
+      <Text style={{fontSize:30,fontWeight:'bold',color:'white',marginLeft:70}}>{props.name}</Text>
     </View>
   )
 }
